baaa764d6587e46ec0f5aaa3afb88b56bd028a09
icos w <wicos@wicos.cn>
2021-08-13 17:03:51 +0800

    something change

---
+++ module/config.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   config.py
+@Time    :   2021/08/13 17:02:18
+@Author  :   Wicos 
+@Version :   1.0
+@Contact :   wicos@wicos.cn
+@Blog    :   https://www.wicos.me
+'''
+
+# here put the import lib
